36894c801d53def40e21aa45dc6a350a2c1a9616
joshinachiket <nachiket.r.joshi@gmail.com>
2017-05-02 01:53:19 -0700

    updating serverside javascript

--- src/Multi-Tenant-SaaS-App-For-Java-UML-Parser/app.js
+++ src/Multi-Tenant-SaaS-App-For-Java-UML-Parser/app.js
@@ -8,6 +8,7 @@ var express = require('express')
   , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
+var index = require('./routes/index');
 
 var app = express();
 
@@ -15,20 +16,15 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/templates/public', express.static('public'))
+app.use(function(req, res, next) {
+	  req.headers['if-none-match'] = 'no-match-for-this';
+	  next();    
+});
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/', index.upload_get);
+app.post('/', index.upload_post);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- src/Multi-Tenant-SaaS-App-For-Java-UML-Parser/routes/index.js
+++ src/Multi-Tenant-SaaS-App-For-Java-UML-Parser/routes/index.js
@@ -1,8 +1,60 @@
-
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+var formidable = require('formidable');
+var fs = require('fs');
+var unzip = require('unzip');
+var exec = require('child_process').exec;
+var errors = require('errors');
+
+var upload_get = function (req, res) {
+	res.render('index');
 };
+
+var upload_post = function (req, res) {
+	
+	console.log("control caught in upload_post");
+	
+	 var form = new formidable.IncomingForm();
+
+	    form.parse(req);
+	    form.on('fileBegin', function (name, file){
+	    	var dir = './public/uploads/';
+	        if (!fs.existsSync(dir)){
+	        	fs.mkdirSync(dir);
+	        }
+	        file.path = dir + file.name;
+	    });
+
+	    form.on('file', function (name, file){
+	    	// Unzip the file
+	    	fs.createReadStream(file.path)
+	    		.pipe(unzip.Extract({ path: './public/uploads/' }))
+	    		.on('close',function(err) {
+	    			
+	    			if (err){
+	    				res.send(new errors.errors.Http500Error());
+	    			} else {
+	    				// run the jar
+		    			var extractedFilePath = file.path.substring(1,file.path.lastIndexOf('.'));
+		    			var outputFile = 'output';
+		    			
+		    			exec('java -jar ./public/uml_parser/UmlParser.jar '+'.'+extractedFilePath+' '+outputFile,
+		    					function (error, stdout, stderr){
+		    				    if(error !== null){
+		    				    	console.log("Error -> "+error);
+		    				    	res.send({'path':'./public/images/server_error.png'});
+		    				    } else {
+		    				    	console.log("Uml Diagram Generate");
+		    				    	var outputFilePath = '.'+extractedFilePath+'/'+outputFile+'.png';
+		    				    	console.log(outputFilePath);
+		    				    	res.send({'path':outputFilePath});
+		    				    }
+		    			});
+	    			}
+	    		});
+	    });
+};
+
+module.exports = { upload_get, upload_post}
