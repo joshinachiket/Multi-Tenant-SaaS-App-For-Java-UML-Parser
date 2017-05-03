164824f58c6af59a742d2dff54977fed50e7ee44
joshinachiket <nachiket.r.joshi@gmail.com>
2017-05-02 23:50:57 -0700

    adding tenant server laura

---
+++ src/tenants/Tenant-Laura-Server/app.js
@@ -0,0 +1,31 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , http = require('http')
+  , path = require('path');
+var index = require('./routes/index');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static('public'))
+
+app.use(function(req, res, next) {
+	  req.headers['if-none-match'] = 'no-match-for-this';
+	  next();    
+});
+
+app.get('/', index.upload_get);
+app.post('/', index.upload_post);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
