5554184de07e24c1b5e7a919e9e1d4796cd55b86
joshinachiket <nachiket.r.joshi@gmail.com>
2017-05-02 23:51:44 -0700

    adding tenant server akshay

---
+++ src/tenants/Tenant-Akshay-Server/public/angular/main.js
@@ -0,0 +1,58 @@
+/**
+ * Tenant-Akshay
+ */
+var myApp = angular.module('app', []);
+
+console.log("control caught in app controller");
+
+myApp.directive('fileModel', ['$parse', function ($parse) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            var model = $parse(attrs.fileModel);
+            var modelSetter = model.assign;
+            
+            element.bind('change', function(){
+                scope.$apply(function(){
+                    modelSetter(scope, element[0].files[0]);
+                });
+            });
+        }
+    };
+}]);
+
+myApp.service('fileUpload', ['$http', function ($http) {
+    this.uploadFileToUrl = function(file, uploadUrl, cb) {
+        var fd = new FormData();
+        fd.append('file', file);
+        
+        console.log("control caught before making HTTP request");
+        
+        
+        $http.post(uploadUrl, fd, {
+            transformRequest: angular.identity,
+            headers: {'Content-Type': undefined}
+        })
+        .success(function(data){
+        	cb(data);
+        })
+        .error(function(err){
+        	console.log(err);
+        	cb(err);
+        });
+    }
+}]);
+
+myApp.controller('myCtrl', ['$scope', 'fileUpload', function($scope, fileUpload){
+	
+	console.log("control caught in myCtrl controller");
+    
+    $scope.uploadFile = function(){
+        var file = $scope.myFile;
+        var uploadUrl = "/";
+        fileUpload.uploadFileToUrl(file, uploadUrl, function(data) {
+        	console.log(data.path);
+        	$scope.output = data.path;
+        });
+    };
+}]);
